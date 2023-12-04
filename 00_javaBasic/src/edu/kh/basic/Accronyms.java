66f3b1ba8f410658cf68429d58c3fc89b4e6c7b8
jakelee0424 <qkql006@gmail.com>
2023-12-04 10:25:08 +0900

    231204

---
+++ 00_javaBasic/src/edu/kh/basic/Accronyms.java
@@ -0,0 +1,24 @@
+package edu.kh.basic;
+
+public class Accronyms {
+
+	
+	// ctrl + F11 : Running
+	// ctrl + alt + 방향키 : 해당 줄 복사
+	// ctrl + D : 해당 줄 제거
+	// ctrl + / : 해당 줄 주석으로
+	// 블럭 설정 + ctrl + shift + / : 해당 블럭 주석으로
+	// 블럭 설정 + ctrl + shift + \ : 해당 블럭 주석 해제 
+	// ctrl + I : 들여쓰기 자동 수정
+	// ctrl + space : 문장 자동 완성
+	// crtl + w : 소스파일 닫기 (crtl + shift + w : 전체)
+	// crtl 클릭 : 해당 문장 추적
+	
+	// ctrl shift + /(num) : 메소드 전제 폴딩
+	// ctrl shift + *(num) : 메소드 전제 언폴딩
+	// ctrl + -(num) : 단일 메소드 폴딩
+	// ctrl + +(num) : 단일 메소드 언폴딩
+
+	//ctrl + tab : 다음 소스창 이동
+	//ctrl + shift + tab 이전 소스창 이동
+}
---
+++ 00_javaBasic/src/edu/kh/basic/JavaTest.java
@@ -0,0 +1,55 @@
+package edu.kh.basic;
+
+
+//(한 줄 주석) - 컴파일러가 해석하지 않는 부분, 주로 코드 설명
+/*(범위 주석) */
+
+//컴파일러(Compiler) - 코드를 2진수(0,1)로 변환하는 번역기
+
+/* Java 코드 실행 순서
+1. 코드작성 
+2. 실행 ( 컴파일러가 2진수로 번역 -> JVM이 실행 )
+*/
+
+public class JavaTest {
+
+	// 클래스(Class) - 자바 코드가 작성되는 영역 
+	
+	public static void main(String[] args) {
+		
+	// 메인 메서드(main method) - 자바 애플리케이션(프로그램)을 실행하기 위해 반드시 필요한 메서드
+		
+		System.out.println("123");		
+		System.out.println("456");		
+		System.out.println("-----------------");		
+		
+		// ctrl + alt + 방향키 : 해당 줄 복사
+		
+		System.out.println(1+2);	
+		System.out.println(5*4);	
+		System.out.println(12%7);	
+		System.out.println(465/5);	
+		System.out.println("1 + 2");
+		System.out.println("-----------------");		
+		
+		// "" 안에 작성된 코드는 단순 문자열로 인식
+		// "" 안에 작성되지 않는 코드는 숫자 or 변수로 인식 
+		
+		System.out.println("1" + 2);
+		System.out.println(1 + "2");
+		System.out.println("1" + "2");
+		System.out.println("14 * 19 = " + 266);
+		System.out.println("14 * 19 = " + 266 + 312);
+		System.out.println("14 * 19 = " + (266 + 312));
+		
+		// 자바는 사칙연산의 우선순위를 그대로 따름
+		// 우선 계산을 원하면 () 사용
+		
+		// '+'의 역할 -> 숫자 + 숫자 = 덧셈 연산 결과
+		//               문자열 + 숫자 or 문자열 + 문자열  = 이어쓰기                   
+		
+	
+	}
+}
+
+
